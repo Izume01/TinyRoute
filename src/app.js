9135d7efc0ce5f5d44adc8f476994986a9a15879
izume01 <shrey16211@gmail.com>
2024-10-12 22:01:35 +0530

    rendered public

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors'
+import cors from 'cors';
+import path from 'path';
 import connectDB from './connectDB.js';
 import urlRoutes from './routes/urlRoutes.js';
 
@@ -14,10 +15,14 @@ connectDB(dbUrl);
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // Add this line
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Add the URL routes to the app
 app.use('/api', urlRoutes);
 
-
-app.listen(port ,()=> {
-    console.log('Server started')
-})
+app.listen(port, () => {
+    console.log('Server started');
+});
