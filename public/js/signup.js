82a86ef85a68ec2a881ad57f63e47bc6bed6aa92
izume01 <shrey16211@gmail.com>
2024-10-14 23:04:48 +0530

    Update package-lock.json and package.json

---
+++ public/js/signup.js
@@ -0,0 +1,40 @@
+const RegistrationPassword = document.querySelector('#password');
+const RegistrationEmail = document.querySelector('#email');
+const RegistrationUsername = document.querySelector('#username');
+
+const RegistrationSumbitbtn = document.querySelector('.Reg-btn');
+
+RegistrationSumbitbtn.addEventListener('click' , async (e)=> {
+    e.preventDefault();
+    const username = RegistrationUsername.value.trim();
+    const email = RegistrationEmail.value.trim();
+    const password = RegistrationPassword.value.trim();
+
+    if (!email || !password || !username) {
+        alert('Please enter all details.');
+        return;
+    }
+
+    try {
+        const response = await fetch('http://localhost:8000/auth/register' ,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name : username,
+                email,
+                password
+            })
+        })
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.message || 'Login failed'); // Throw an error for non-200 responses
+        }
+        const data = await response.json();
+
+    } catch (error) {
+        console.log(error);
+    }
+})
